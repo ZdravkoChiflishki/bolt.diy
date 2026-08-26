03d55bb18df761fd8b47c4bc34c3881a42f12fa1
Zdravko Chiflishki <ZdravkoChiflishki@outlook.com>
2026-08-26 22:41:07 +0300

    fix: keep cloudflare function static import

--- functions/[[path]].ts
+++ functions/[[path]].ts
@@ -2,8 +2,7 @@ import type { ServerBuild } from '@remix-run/cloudflare';
 import { createPagesFunctionHandler } from '@remix-run/cloudflare-pages';
 
 export const onRequest: PagesFunction = async (context) => {
-  const serverBuildPath = '../build/server';
-  const serverBuild = (await import(serverBuildPath)) as unknown as ServerBuild;
+  const serverBuild = (await import('../build/server')) as unknown as ServerBuild;
 
   const handler = createPagesFunctionHandler({
     build: serverBuild,
